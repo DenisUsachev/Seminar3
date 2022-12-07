a75d92df049841ff2f2ae9bf0e8f0e1b045bb9f8
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-07 21:54:43 +0500

    add new repo

---
+++ tesk2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Напишите программу, которая по заданному номеру четверти,
+показывает диапазон возможных координат точек в этой четверти (x и y).*/
+
+//Нахождение через метод switch
+string Method(int quarter)
+{
+    switch(quarter)
+    {
+        case 1:
+            return"x = 0 ... ∞ \ny = 0 ... ∞";
+            break;
+        case 2:
+            return"x = 0 ... -∞ \ny = 0 ... ∞";
+            break;
+        case 3:
+            return"x = 0 ... -∞ \ny = 0 ... -∞";
+            break;
+        case 4:
+            return"x = 0 ... ∞ \ny = 0 ... -∞";
+            break;
+        default:
+            return "Нужно ввести число от 1 до 4 (четверть)"
+    }
+    
+    
+    if(quarter < 0 && quarter < 5)
+    {
+        
+    }
+    else
+        return "Нужно ввести число от 1 до 4(четверть)";
+}
