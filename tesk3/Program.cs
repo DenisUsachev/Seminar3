1e34e04a181605c31415b6595910c4d255866c6f
DenisUsachev <114069393+DenisUsachev@users.noreply.github.com>
2022-12-08 18:21:25 +0500

    add new commit

--- tesk3/Program.cs
+++ tesk3/Program.cs
@@ -6,21 +6,24 @@ A (7,-5); B (1,-1) -> 7,21
 */
 
 Console.WriteLine("Введите координаты X первой точки");
-int x1 = Console.ReadLine();
+int x1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите координаты Y первой точки");
-int y1 = Console.ReadLine();
+int y1 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите координаты X второй точки");
-int x2 = Console.ReadLine();
+int x2 = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите координаты Y второй точки");
-int y2 = Console.ReadLine();
+int y2 = Convert.ToInt32(Console.ReadLine());
 
 double DistXY (int x1, int y1, int x2, int y2)
 {
-    double result =0;
+    double result = 0;
     result = Math.Sqrt((Math.Pow(x1 - x2 ),2) + (Math.Pow(y1 - y2 ),2));
-
+    /*Math.Sqrt - это корень*/
+    /*Math.Pow - возведение в степень*/
     return result;
 }
+
+Console.WriteLine($"Расстояние между точками = {DistXY(x1,y1,x2,y2)}"); 
